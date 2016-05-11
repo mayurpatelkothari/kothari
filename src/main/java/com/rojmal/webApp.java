cee6caa594a0269483484f7cb1cc0e1d102f623b
Mayur <kotharimayur9@gmail.com>
2016-05-11 07:17:01 -0700

    add webApp

--- src/main/java/com/rojmal/webApp.java
+++ src/main/java/com/rojmal/webApp.java
@@ -1,10 +1,9 @@
 package com.rojmal;
 
-import java.util.UUID;
-
+import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.IdGenerator;
 
 @SpringBootApplication
 public class webApp
@@ -13,12 +12,15 @@ public class webApp
 
     public static void main(String[] args)
     {        
+    	
+    	System.out.println("application start");
+
 //        logger.debug("main() :: application start");
     	System.out.println("application stated");
         SpringApplication.run(webApp.class, args);
 //        logger.debug("main() :: application end");
     	System.out.println("application close");
-    	
+    
     	
     }
 }
